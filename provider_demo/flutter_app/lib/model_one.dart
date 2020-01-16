462fa29ebac71235d569fc0f8a2908342b267952
waitwalker <waitwalker@163.com>
2020-01-16 10:47:38 +0800

    add model one

---
+++ provider_demo/flutter_app/lib/model_one.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ModelOne with ChangeNotifier {
+
+  int _count = 0;
+  int get value => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+}
