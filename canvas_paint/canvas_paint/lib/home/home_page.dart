33ece371271004639dd59c6ef7ae4d8379814b1b
waitwalker <waitwalker@163.com>
2020-03-07 11:43:31 +0800

    主页创建

---
+++ canvas_paint/canvas_paint/lib/home/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+///
+/// @ClassName HomePage
+/// @Description 
+/// @Author waitwalker
+/// @Date 2020-03-07
+///
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
