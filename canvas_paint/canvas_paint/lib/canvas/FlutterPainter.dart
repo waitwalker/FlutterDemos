8e8c68adcdc09fbcca508a3f44484072d0200ac0
waitwalker <waitwalker@163.com>
2020-03-07 17:23:18 +0800

    注释添加

--- canvas_paint/canvas_paint/lib/canvas/FlutterPainter.dart
+++ canvas_paint/canvas_paint/lib/canvas/FlutterPainter.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-
+///
+/// @ClassName FlutterPainter
+/// @Description 绘制类
+/// @Author waitwalker
+/// @Date 2020-03-07
+///
 class FlutterPainter extends CustomPainter {
-
   @override
   void paint(Canvas canvas, Size size) {
+    /// 初始化画笔
     var paint = Paint()
-        ..strokeWidth = 25.0
-        ..color = Colors.red;
+      ..strokeWidth = 25.0
+      ..color = Colors.red;
+
+    /// 通过canvas画一条直线
     canvas.drawLine(Offset(95, 0), Offset(95, 300), paint);
   }
 
